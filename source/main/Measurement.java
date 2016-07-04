1f66579cdbbd6d68c1be6528544f54872bf614be
Lottie Carruthers <lottie.carruthers@gmail.com>
2016-07-04 19:42:42 +0100

    update measurement to use previous commit

--- source/main/Measurement.java
+++ source/main/Measurement.java
@@ -19,11 +19,17 @@ public class Measurement {
                     "hello5="
                             + "THIS@HEAD:"
                             + "main."
+                            + "HelloWorld#hello5(String)",
+                    "hello5tr-1="
+                            + "THIS@tr-1:"
+                            + "main."
                             + "HelloWorld#hello5(String)"
             },
             formula = {
                     " for (n {100, 1000, 10000})"
-                    + "hello5[generator](n) < hello4[generator](n)"
+                    + "hello5[generator](n) < hello4[generator](n)",
+                    "for (n {10000})"
+                    + "hello5[generator](n) = hello5tr-1[generator](n)"
             })
     
     public static void main(String args[]) {
